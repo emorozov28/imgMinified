7133c700a0dc00bc57a3dbffdbb846d8b03dc64c
emorozov28 <emorozov328@gmail.com>
2021-07-01 10:58:16 +0300

    init

---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const path = require('path');
+const Jimp = require('jimp');
+const cc = require("node-console-colors");
+
+const pathImg = path.resolve(__dirname, 'img');
+const pathBuild = path.resolve(__dirname, 'build');
+
+fs.stat(pathBuild, (err) => {
+    if (!err) {
+        clearBuild();
+        startMinified();
+    }
+    else if (err.code === 'ENOENT') {
+        fs.mkdirSync(pathBuild);
+        clearBuild();
+        startMinified();
+    }
+});
+
+
+const clearBuild = () => {
+    fs.readdir(pathBuild, (err, files) => {
+
+        if (files.length > 0) {
+            files.forEach(file => {
+                fs.unlink(path.resolve(__dirname, 'build', file), (err) => {
+                    //console.log(err)
+                });
+            });
+        } else {
+            console.log('No images found')
+        }
+    });
+}
+
+const startMinified = () => {
+    fs.readdir(pathImg, (err, files) => {
+        const max = 100;
+        let process = 0;
+        const iterator = 100 / files.length;
+        files.forEach(file => {
+            Jimp.read(path.resolve(__dirname, pathImg, file), (err, images) => {
+                if (err) throw err;
+                images
+                    // .resize(1920, 1080, Jimp.RESIZE_BEZIER) // resize
+                    .quality(50) // set JPEG quality
+                    // .greyscale() // set greyscale
+                    .scale(0.5)
+                    .write(path.resolve(__dirname, 'build', file)) // save
+                    // .resize(Jimp.AUTO, 250);
+                process += iterator;
+                console.log(cc.set('fg_green', Math.round(process) + '%'));
+                if (process == 100) {
+                    console.log(cc.set('fg_black', 'bg_green', 'Process successful'));
+                }
+            });
+        });
+    });
+}
